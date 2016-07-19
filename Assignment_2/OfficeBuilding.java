8392991d23e70a098fe298bdb9e280430e75949d
AgilanSelvaraj <agilan.ks07@gmail.com>
2016-07-19 19:32:57 +0530

    Add files via upload

---
+++ Assignment_2/OfficeBuilding.java
@@ -0,0 +1,10 @@
+package OOPS_Part3.Assignment_2;
+
+public class OfficeBuilding extends RawMaterials{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+	
+
+}
---
+++ Assignment_2/RawMaterials.java
@@ -0,0 +1,19 @@
+package OOPS_Part3.Assignment_2;
+
+public class RawMaterials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int NoOfDays, NoOfLabours;
+		String BuildingType;
+
+	}
+	
+	public int getArea(int l, int b){
+		//int l,b;
+		int area=l*b;
+		return area;
+	}
+
+}
